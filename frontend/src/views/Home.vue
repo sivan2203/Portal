5c97de993bb303eecf5b8493b275132381240cf2
Just_PlayBoy <urovjohn@gmail.com>
2021-04-14 23:50:55 +0700

    Начал разработку одного поста

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,17 +1,31 @@
 <template>
-  <div class="home">
-    <b-button variant="primary">good</b-button>
+  <div  class="bg-primary text-dark" style="margin-top: 1%; margin: 0 auto; width: 60%; border-radius: 5px">
+        <b-card 
+          title="Card Title"
+          img-src="https://picsum.photos/600/300/?image=25"
+          img-alt="Image"
+          img-top
+          tag="article"
+          class="mb-2"
+        >
+        <b-card-text>
+          Some quick example text to build on the card title and make up the bulk of the card's content.
+        </b-card-text>
+        <b-button variant="primary">Read more</b-button>
+        </b-card>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import { BButton } from 'bootstrap-vue'
+import { BButton, BCardText, BCard, } from 'bootstrap-vue'
 
 export default {
   name: 'Home',
   components: {
-    BButton
+    BButton,
+    BCardText, 
+    BCard,
   }
 }
 </script>
