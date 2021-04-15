1b2ab696c1b6892dd6ec7d3430a0fb0d594a7631
Just_PlayBoy <urovjohn@gmail.com>
2021-04-15 22:03:49 +0700

    Начал подключение препроцессора scss

--- frontend/src/views/AdminPanel.vue
+++ frontend/src/views/AdminPanel.vue
@@ -1,24 +1,23 @@
 <template>
-  <div class="about" style="margin: 0 auto; width: 60%">
+  <div class="about" style="margin: 0 auto; width: 40%">
     <h1>This is an Admin page</h1>
-    <b-card>
-    <b-form-input type="text" placeholder="hi"/>
-    <b-button variant="primary">Create News</b-button>
-    <b-img />
-    <b-form-file />
+    <b-card
+    img-src="https://picsum.photos/600/300/?image=25">
+      <b-form-input type="text" placeholder="Title of news"/>
+      <b-form-file />
       <b-form-textarea></b-form-textarea>
+      <b-button variant="primary">Create News</b-button>
     </b-card>
   </div>
 </template>
 
 <script>
-import { BCard, BFormTextarea, BImg, BFormInput, BFormFile, BButton, } from 'bootstrap-vue'
+import { BCard, BFormTextarea, BFormInput, BFormFile, BButton, } from 'bootstrap-vue'
 
 export default ({
   components: {
     BCard, 
     BFormTextarea, 
-    BImg, 
     BFormInput, 
     BFormFile,
     BButton,
