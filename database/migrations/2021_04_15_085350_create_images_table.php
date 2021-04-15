7d89834b7b98cb557ccee03f11d61d15d07128a3
sivan2203 <sivan9458@gmail.com>
2021-04-15 16:10:31 +0700

    #11 create images table and migration

--- database/migrations/2021_04_15_085350_create_images_table.php
+++ database/migrations/2021_04_15_085350_create_images_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCommentsTable extends Migration
+class CreateImagesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,18 +13,16 @@ class CreateCommentsTable extends Migration
      */
     public function up()
     {
-        Schema::create('comments', function (Blueprint $table) {
+        Schema::create('images', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('article_id')->unsigned();
-            $table->integer('autor_id')->unsigned();
-            $table->text('body');
+            $table->binary('image');
             $table->timestamps();
 
 
         });
-        Schema::table('comments', function(Blueprint $table) {
+        Schema::table('images', function(Blueprint $table) {
             $table->foreign('article_id')->references('id')->on('articles');
-            $table->foreign('autor_id')->references('id')->on('people');
         });
     }
 
@@ -35,6 +33,7 @@ class CreateCommentsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('comments');
+        Schema::dropIfExists('images');
     }
 }
+
