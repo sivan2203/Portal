85a2ec2dcaef9e1608703d213b2344fdc2e8cd7a
sivan2203 <sivan9458@gmail.com>
2021-04-15 16:53:36 +0700

    #13 create ImagesController

--- app/Http/Controllers/Images/ImagesController.php
+++ app/Http/Controllers/Images/ImagesController.php
@@ -3,9 +3,71 @@
 namespace App\Http\Controllers\Images;
 
 use App\Http\Controllers\Controller;
+use App\Models\ImagesModel;
 use Illuminate\Http\Request;
 
 class ImagesController extends Controller
 {
-    //
+    public function images(){
+        return response()->json(ImagesModel::get(), 200);
+    }
+
+    public function ImagesById($id){
+        $images = ImagesModel::find($id);
+        if(is_null($images)){
+            return response()->json(['error' => true, 'message' => 'Not found'], 404);
+        }
+        return response()->json($images, 200);
+    }
+
+    public function ImagesByArticleId($article_id){
+        $images = ImagesModel::find($article_id);
+        if(is_null($images)){
+            return response()->json(['error' => true, 'message' => 'Not found'], 404);
+        }
+        return response()->json($images, 200);
+    }
+
+    public function imagesSave(Request $req){
+        $images = ImagesModel::create($req->all());
+        return response()->json($images, 201);
+    }
+
+    public function imagesEditById(Request $req, $id){
+
+        $images = ImagesModel::find($id);
+        if(is_null($images)){
+            return response()->json(['error' => true, 'message' => 'Not found'], 404);
+        }
+        $images->update($req->all());
+        return response()->json($images, 200);
+    }
+
+    public function imagesEditByArticleId(Request $req, $article_id){
+
+        $images = ImagesModel::find($article_id);
+        if(is_null($images)){
+            return response()->json(['error' => true, 'message' => 'Not found'], 404);
+        }
+        $images->update($req->all());
+        return response()->json($images, 200);
+    }
+
+    public function imagesDeleteById(Request $req, $id){
+        $images = ImagesModel::find($id);
+        if(is_null($images)){
+            return response()->json(['error' => true, 'message' => 'Not found'], 404);
+        }
+        $images->delete();
+        return response()->json('', 204);
+    }
+
+    public function imagesDeleteByArticleId(Request $req, $article_id){
+        $images = ImagesModel::find($article_id);
+        if(is_null($images)){
+            return response()->json(['error' => true, 'message' => 'Not found'], 404);
+        }
+        $images->delete();
+        return response()->json('', 204);
+    }
 }
