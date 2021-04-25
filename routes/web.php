8d5df1c3cfadccd24481841a29bfec0a14c7d3a2
sivan2203 <sivan9458@gmail.com>
2021-04-25 23:17:09 +0700

    make PhotoController

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('photo/{hashname}', 'PhotosController@showPhoto')->name('showPhoto');
