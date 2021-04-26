5e082ce4693f1924b6a1dc2bb4d5212c1c1e13b8
Just_PlayBoy <urovjohn@gmail.com>
2021-04-26 23:02:54 +0700

    Отредактировал панель администратора

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,7 +1,12 @@
 <template>
-  <div :optios="test" class="text-dark" style="margin-top: 1%; margin: 0 auto; width: 40%; border-radius: 5px">
+  <div
+    :optios="test"
+    class="text-dark"
+    style="margin-top: 1%; margin: 0 auto; width: 40%; border-radius: 5px"
+  >
     <b-card
-      v-for="(title, item) in textNews" :key="item"
+      v-for="(title, item) in textNews"
+      :key="item"
       :title="title.title"
       img-src="https://picsum.photos/600/300/?image=25"
       img-alt="Image"
@@ -14,55 +19,64 @@
           {{ title.content }}
         </b-card-text>
       </b-collapse>
-      <b-button v-b-toggle="`collapse${title.id}`" variant="primary">See more</b-button>
+      <b-button v-b-toggle="`collapse${title.id}`" variant="primary"
+        >See more</b-button
+      >
     </b-card>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import { BButton, BCardText, BCard, BCollapse, } from 'bootstrap-vue'
+import { BButton, BCardText, BCard, BCollapse } from 'bootstrap-vue'
 import axios from 'axios'
 
 export default {
   components: {
     BButton,
-    BCardText, 
+    BCardText,
     BCard,
-    BCollapse,
+    BCollapse
   },
   data() {
-    return ({
+    return {
       textNews: [
-        {id: 1, title: 'Card Title', content: `Some quick example text to build on the card title and make up the bulk of the card's content.`},
-         {id: 2, title: 'Lorem Title', content: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Provident odit dolorum sint aperiam, ipsa consequatur nesciunt alias omnis voluptas libero perspiciatis ipsam saepe labore velit accusantium veniam illo, voluptatem adipisci.`}
-      ],
-      testArr: [
-        {id: '3', title: 'ref', content: 'her'},
-        {id: '4', title: 'fer', content: 'reg'}
+        {
+          id: 1,
+          title: 'Card Title',
+          content:
+            "Some quick example text to build on the card title and make up the bulk of the card's content."
+        },
+        {
+          id: 2,
+          title: 'Lorem Title',
+          content: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Provident odit dolorum sint aperiam, ipsa consequatur nesciunt alias omnis voluptas libero perspiciatis ipsam saepe labore velit accusantium veniam illo, voluptatem adipisci.`
+        }
       ]
-    })
+    }
   },
   computed: {
-    test(){
+    test() {
       return this.getNewsData()
     }
   },
   methods: {
     getNewsData() {
-      axios.get('/api/news/all').then(response => {
-        response.data.forEach(element => {
-          this.textNews.push({id: element.id, title: element.title, content: element.content})
+      axios
+        .get('http://localhost:8000/api/news/all')
+        .then(response => {
+          response.data.forEach(element => {
+            this.textNews.push({
+              id: element.id,
+              title: element.title,
+              content: element.content
+            })
+          })
         })
-      }).catch(error => {
-        console.log(error)
-      })
-    },
-    // testNewsData() {
-    //   this.testArr.forEach(element => {
-    //     this.textNews.push({id: element.id, title: element.title, content: element.content})
-    //   })
-    // }
+        .catch(error => {
+          console.log(error)
+        })
+    }
   }
 }
 </script>
